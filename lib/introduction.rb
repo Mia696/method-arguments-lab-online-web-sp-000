680f299f8e1b5fb06a239c5a33774734c24dd3fc
Mia <tamiapaschall@gmail.com>
2019-11-18 01:18:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,5 @@ def introduction_with_language (name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language ("Josh", "Elixir")
+introduction_with_language "Josh", "Elixir"
 
